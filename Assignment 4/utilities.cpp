7ba37056b37425e900d9b1c068da3544867a789d
eliasoa <eliasoa@stud.ntnu.no>
2023-02-10 15:28:48 +0100

    Problem 3d finished

--- Assignment 4/utilities.cpp
+++ Assignment 4/utilities.cpp
@@ -27,7 +27,44 @@ void printStudent(Student student){
     << endl;
 }
 
-bool isInProgram(Student student, string studyProgram){
+bool isInProgram(Student& student, string& studyProgram){
     if (student.studyProgram == studyProgram) return true;
     else return false;    
+}
+
+string randomizeString(int numChars, char lowerBound, char upperBound){
+    std::random_device rd;
+    std::default_random_engine generator(rd());
+    std::uniform_int_distribution<int> distribution(static_cast<int>(lowerBound),static_cast<int>(upperBound));
+    string ranodomString = "";
+    for (int i = 0; i < numChars; i++)
+    {
+        int number = distribution(generator);
+        ranodomString.push_back(static_cast<char>(number));
+    }
+    return ranodomString;
+}
+
+string readInputToString(const int& length, char lowerBound, char upperBound){
+    char userInput;
+    string returnString = "";
+    cout << "Write a charachter, then press Enter. Do this " << length << " times." << endl;
+    for (int i = 0; i < length; i++)
+    {
+        cin >> userInput;
+        userInput = toupper(userInput);
+               
+        while (userInput < lowerBound)
+        {
+            cout << "Character is too low. Try again: ";
+            cin >> userInput;
+        }
+        while (userInput > upperBound)
+        {
+            cout << "Character is too high. Try again: ";
+            cin >> userInput;
+        }
+        returnString.push_back(userInput);
+    }
+    return returnString;
 }
