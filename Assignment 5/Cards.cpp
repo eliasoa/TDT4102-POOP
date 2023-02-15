17d3e9533205b27c5be4a126b098bd16c6d7f348
eliasoa <eliasoa@stud.ntnu.no>
2023-02-15 22:13:40 +0100

    Finished task 3a

--- Assignment 5/Cards.cpp
+++ Assignment 5/Cards.cpp
@@ -1,5 +1,23 @@
 #include "Cards.h"
 
-string suitToString(enum suit){
-    return "ligma";
+string suitToString(const Suit& suit){
+    return suitStringMap.at(suit);
+}
+
+string rankToString(const Rank& rank){
+    return rankStringMap.at(rank);
+}
+
+Card::Card(Suit suit, Rank rank): s{suit}, r{rank} {};
+
+Suit Card::getSuit(Suit suit){
+    return s;
+}
+
+Rank Card::getRank(Rank rank){
+    return r;
+}
+
+string Card::toString(){
+    return rankToString(r) + " of " + suitToString(s);
 }
--- Assignment 5/main.cpp
+++ Assignment 5/main.cpp
@@ -1,6 +1,21 @@
 #include "Cards.h"
+
 int main(){
-    // cout << suitToString(Suit::spades);
-    cout << static_cast<int>(Suit::spades) << endl;
+    Card c{Suit::clubs, Rank::four};
+    cout << c.toString() << endl;
     return 0;
 }
+
+
+// Rank r = Rank::king;
+// Suit s = Suit::hearts;
+// string rank = rankToString(r);
+// string suit = suitToString(s);
+// cout << "Rank: " << rank << " Suit: " << suit << '\n';
+
+/* Teori
+Oppgave 1e)
+To fordeler ved å bruke symboler fremfor f.eks. heltall og strenger
+-   Universalt
+-   Enklere kode?
+*/
