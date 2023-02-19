1321da1068ee801784cdd280f6628e44a0ce3a30
eliasoa <elias.olsen.almenningen@gmail.com>
2023-02-19 18:08:56 +0100

    Trying with visuals

--- Assignment 4/main.cpp
+++ Assignment 4/main.cpp
@@ -1,56 +1,13 @@
 #include "utilities.h"
 #include "tests.h"
 #include "mastermind.h"
+#include "masterVisual.h"
+
 int main() {
     playMastermind();
+    // playMastermindVisual();
     return 0;
 }
-
-
-// int userInput = 0;
-    // bool terminate = false;
-    // // temp variables
-    // int a = 0;
-    // int b = 0;
-    // while (terminate != true)
-    // {   
-    //     cout << "Input: ";
-    //     cin >> userInput;
-    //     switch (userInput)
-    //     {
-    //     case 0:
-    //         terminate = true;
-    //         cout << "Exiting" << endl;
-    //         break;
-    //     case 1:
-    //         testCallByValue();
-    //         break;
-    //     case 2:
-    //         testCallByReference();
-    //         break;
-    //     case 3:
-    //         a = 1;
-    //         b = 2;
-    //         swapNumbers(a,b);
-    //         cout << "a: " << a << "b: " << b << endl;
-    //         break;    
-    //     case 4:
-    //     {
-    //         Student studentOne {"Elias", "Kybernetikk og Robotikk", 24};
-    //         Student studentTwo {"Per Magnus", "Elsys", 24};
-    //         printStudent(studentOne);
-    //         printStudent(studentTwo);
-    //         cout << isInProgram(studentTwo, "Elsys") << endl;
-    //         cout << isInProgram(studentOne, "Elsys") << endl;
-    //     }
-    //         break;
-    //     case 5:
-    //         testString();
-    //         break;
-    //     default:
-    //         break;
-    //     }  
-    // }    
 
 /* Teorispørsmål
 Oppgave 1:
