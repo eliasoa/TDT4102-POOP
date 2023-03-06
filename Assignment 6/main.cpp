3c4c1ec787d442e35cc64d9eb9eb5b2690ef7a52
eliasoa <eliasoa@stud.ntnu.no>
2023-03-06 17:53:14 +0100

    made assignment presentable

--- Assignment 6/main.cpp
+++ Assignment 6/main.cpp
@@ -1,30 +1,5 @@
 #include "includes.h"
 
-
-int main() {
-    // CourseCatalog c;
-    // c.addCourse("TDT4110","Informasjonsteknologi grunnkurs");
-    // c.addCourse("TDT4102","Prosedyre- og objektorientert programmering");
-    // c.addCourse("TMA4100", "Matematikk 1");
-    // cout << c << endl;
-    // c.exportCourseCatalog("c");
-    // c.loadCourseCatalog("courseCatalog.csv");
-    // cout << c << endl;
-
-    // std::filesystem::path temperatureFile{"temperatures.txt"};
-    // std::ifstream temp_file{temperatureFile};
-    // Temps t;
-    // temp_file >> t; 
-    // cout << t.max << t.min << endl;
-
-    vector<Temps> nils = readTemps("temperatures.txt");
-
-    return 0;
-}
-
-
-/* Teorioppgaver
-2b) 
 const map<string, string> capitalsMap {
     {"Norway", "Oslo"},
     {"Sweden", "Stockholm"},
@@ -33,6 +8,50 @@ const map<string, string> capitalsMap {
 string getCapital(const string& country) {
     return capitalsMap.at(country);
 }
+
+int main() {
+
+    // Problem 1b
+    // readFromFileAndCreatNewOneWithLineNumbers("TheTragedyofDarthPlagueistheWise.txt");
+
+    // Problem 2a
+    // characterStatisticsOfFile("grunnlov.txt");
+
+    // Problem 2b
+    // std::cout << "Capitals:" << std::endl;
+    // for (pair<const string, const string> elem : capitalsMap) {
+    //     cout << getCapital(elem.first) << std::endl;
+    // }
+
+    // Problem 3
+    // CourseCatalog c;
+    // c.addCourse("TDT4110","Informasjonsteknologi grunnkurs");
+    // c.addCourse("TDT4102","Prosedyre- og objektorientert programmering");
+    // c.addCourse("TMA4100", "Matematikk 1");
+    // cout << c << endl;
+    
+    // c.removeCourse("TDT4102");
+    // cout << c << endl;
+
+    // c.addCourse("TDT4102","Prosedyre- og objektorientert programmering");
+    // c.exportCourseCatalog("c");
+    // c.loadCourseCatalog("courseCatalog.csv");
+    // cout << c << endl;
+
+    // Problem 4
+    // std::filesystem::path temperatureFile{"temperatures.txt"};
+    // std::ifstream temp_file{temperatureFile};
+    // Temps t;
+    // temp_file >> t; 
+    // cout << t.max << t.min << endl;
+
+    return 0;
+}
+
+
+/* Teorioppgaver
+2b) 
+
 int main(){ 
     std::cout << "Capitals:" << std::endl;
     for (pair<const string, const string> elem : capitalsMap) {
